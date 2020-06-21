763088df22e56957c0cd92f96629f000acbaddd4
Sangsaka Wira <sangsakawira@gmail.com>
2020-06-21 07:02:27 +0700

    classes function for inference added

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 # Load package for inference and flask library
-from numpy import loadtxt
-from keras.models import load_model
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 from flask import Flask,jsonify
 import numpy as np
 import pandas as pd
 from flask_cors import CORS,cross_origin
 from flask import request
+from numpy import loadtxt
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
 import json 
 
+UPLOAD_FOLDER = "uploads"
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 CORS(app)
@@ -16,7 +21,27 @@ UPLOAD_FOLDER = '/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
  
 # load model
-model = load_model('model_fp59.h5')
+model = load_model('model_fp_59.h5')
+ 
+# load and prepare the image
+def load_image(filename):
+	# load the image
+	img = load_img(filename, target_size=(128, 128))
+	# convert to array
+	img = img_to_array(img)
+	# reshape into a single sample with 3 channels
+	img = img.reshape(1, 128, 128, 3)
+	# center pixel data
+	img = img.astype('float32')
+	img = img - [123.68, 116.779, 103.939]
+	return img
+
+# load and return class
+def load_predict(predicted):
+    index = predicted.index(1)
+    classes = ["Melanocytic Nevi","Melanoma","Benign Keratosis-like Lesions","Basal Cell Carcinoma","Actinic Keratoses","Vascular Lesions","Dermatofibroma","Squamous Cell Carcinoma"]
+    result = classes[index]
+    return result
 
 @app.route("/predict")
 def predict():
@@ -42,6 +67,9 @@ def predict_post():
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                image = load_image("uploads/"+filename)
+                model.predict(image)
+
             
         return jsonify({'result':predicted[0],'msg':'success'})
     except ValueError:
